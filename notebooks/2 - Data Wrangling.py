87736b35304bcfc8846516373b9d0ed1c17b930c
mgu <dunarand@protonmail.com>
2026-01-02 12:37:47 +0300

    imputation of missing values (cabin is still missing)
    Minor changes to the sections prior to missing values. Removed the sql
    querry from assets and executed sql directly within the notebook.
    Imputation of the `cabin` column is still missing, which we'll deal with
    next.

--- notebooks/2 - Data Wrangling.py
+++ notebooks/2 - Data Wrangling.py
@@ -36,7 +36,11 @@
 # ## Imports
 
 # %%
+import sqlite3
+
 import pandas as pd
+
+conn = sqlite3.connect(":memory:")
 
 df = pd.read_csv("../data/raw/train.csv", encoding="utf-8")
 df.head()
@@ -73,9 +77,9 @@ df[["survived", "pclass", "age", "sibsp", "parch", "fare"]].agg(["min", "max"])
 
 # %% [markdown]
 # We see no invalid values among these columns except for `fare`, where there
-# exist rows with a `fare` amount of 0. These can be real values since a
-# ticket could be handed for free, so we'll check these when we're dealing
-# with outliers. For now, all values seem to be valid.
+# exist rows with a `fare` amount of 0. We shall validate the `fare` column.
+#
+#
 #
 # Let's also validate the `cabin` column. During the understanding phase, we've
 # discovered that this column includes quite a lot of missing values. Before we
@@ -103,8 +107,16 @@ df[["survived", "pclass", "age", "sibsp", "parch", "fare"]].agg(["min", "max"])
 # In our dataset, we see the following cases:
 
 # %%
-cabins = df.loc[df["cabin"].notna(), "cabin"].unique()
-cabins[:70]
+df.to_sql("titanic", conn, if_exists="replace", index=False)
+
+query = """
+SELECT DISTINCT(cabin) FROM titanic
+WHERE cabin IS NOT NULL
+LIMIT 30
+"""
+
+result = pd.read_sql_query(query, conn)
+print(result)
 
 # %% [markdown]
 #   - Cabin numbers start with a capital English letter
@@ -171,7 +183,7 @@ def extract_deck(cabin: str) -> str | list:
 
 
 # %%
-df[df["cabin"].notna()]["cabin"].map(extract_deck).unique()
+df.loc[df["cabin"].notna(), "cabin"].map(extract_deck).unique()
 
 # %% [markdown]
 # We see that only F G000 and F E000 types of cabin numbers exist, while there
@@ -203,10 +215,7 @@ def extract_deck_sectionless(cabin: str) -> str | list:
         return lst[0][0]
     if all([lst[0][0] == x[0] for x in lst]):
         return lst[0][0]
-    if lst[0][0] == "F" and lst[1][0] in [
-        "G",
-        "E",
-    ]:
+    if lst[0][0] == "F" and lst[1][0] in ["G", "E"]:
         return f"F"  # Only this line is modified
     return lst
 
@@ -240,23 +249,39 @@ mismatched[["passengerid", "pclass", "deck", "cabin"]]
 # can extract passenger titles from the `name` column. To do that, we need to
 # extract what titles are present in the dataset. What makes it easy in our
 # case is that titles end with a dot at the end of the word, so we can filter
-# by using that.
+# by using that. Also, the names are consistent in formatting. Every name
+# string starts with the surname, followed by the title with a dot at the end,
+# and then the first and middle names follow. For example, Ward, Miss. Anna.
 
 # %%
-titles = []
-for index, row in df.iterrows():
-    for _str in row["name"].split(" "):
-        if "." in _str and _str not in titles:
-            titles.append(_str)
-titles
+titles = [
+    df["name"]
+    .dropna()
+    .str.split()
+    .explode()
+    .loc[lambda s: s.str.contains(".", regex=False)]
+    .unique()
+    .tolist()
+]
+
+print(titles)
 
 # %% [markdown]
 # Apart from one mistake in the titles ("L."), the rest of them are valid
 # titles in English, French, Italian, etc. The "L." comes from a shortened name
 # in the dataset. Below is an SQL query for the string "L."
-#
-# ![SQL Name column querry](../assets/sql_name_querry.png)
-#
+
+
+# %%
+df.to_sql("titanic", conn, if_exists="replace", index=False)
+query = """
+SELECT NAME FROM TITANIC
+WHERE NAME LIKE "% L. %"
+"""
+result = pd.read_sql_query(query, conn)
+print(result)
+
+# %% [markdown]
 # These titles can be explained as follows:
 # - "Mr.": for men, regardless of marital status, who do not have another
 # professional or academic title
@@ -317,7 +342,7 @@ titles = [
 ]
 
 
-def extract_title(name: str) -> str:
+def extract_title(name: str) -> str | None:
     """
     Extracts the honorific title from the name string
 
@@ -328,22 +353,32 @@ def extract_title(name: str) -> str:
 
     Returns
     -------
-    str
+    str | None
         Honorific title
     """
     str_list = name.split(" ")
     for _str in str_list:
         if _str in titles:
             return _str
-    return ""
+    return None
 
 
 # %%
 df["title"] = df["name"].apply(extract_title)
 
+print(df["title"].isna().sum())
 df[["name", "title"]].head(10)
 
 # %% [markdown]
+# Let's also do a sanity check.
+
+# %%
+df["title"].isna().sum()
+
+# %% [markdown]
+# We understand that every name entry in the database contains a title and that
+# we had no errors extracting these titles.
+#
 # Now, we can cross-reference titles with their respective genders. Note that
 # some of the titles in our dataset are gender-neutral. Thus, we'll only check
 # for the ones that are gender-specific initially.
@@ -359,3 +394,144 @@ pd.crosstab(df["sex"], df["title"])
 # project and will not be needed for our purposes. Therefore, we deem that
 # we've verified the `sex` column using the passenger titles we've extracted
 # from their names.
+
+
+# %% [markdown]
+# ## Missing Values
+
+# %% [markdown]
+# Now that we've validated our data, let's proceed with filling the missing
+# values in.
+
+# %%
+na_counts = df.isna().sum()
+na_counts[na_counts > 0]
+
+# %% [markdown]
+# ### Embarked column
+
+
+# %% [markdown]
+# We'll start with the `embarked` column. Referring back to the data dictionary
+# we've provided in the notebook `Understanding & Planning`, `embarked` column
+# refers to the port of embarkation, meaning where they've boarded the Titanic.
+# The three possible values this column takes are `C` for Cherbourg, France,
+# `Q` for Queenstown, Ireland, and `S` for Southampton, England.
+#
+# > Titanic first departed from Southampton, England. It made two port calls,
+# the first being Queenstown, Ireland, and the second being Cherbourg, France.
+# This route was popular among British ocean liners for the Southmapton-New
+# York route.
+# >
+# > *"Titanic's maiden voyage was intended to be the first of many
+# trans-Atlantic crossings between Southampton and New York via Cherbourg and
+# Queenstown on westbound runs."*
+# >
+# > (*Wikipedia contributors, 2025*).
+#
+# Let's first see the passengers with missing boarding locations and then try
+# to make some connections for filling these missing values.
+
+# %%
+df[df["embarked"].isna()]
+
+# %% [markdown]
+# Both passengers have first class tickets for cabin "B28" and they share the
+# same ticket number. Therefore, they've traveled together. In this case, it
+# makes sense that they also boarded the liner together.
+
+# %%
+df[df.pclass == 1].groupby(by="embarked")["embarked"].value_counts()
+
+# %% [markdown]
+# We see that the vast majority of the passengers with `pclass` as 1 embarked
+# from Southampton, followed by Cherbourg.
+#
+# Considering the vast majority of 1st class passengers boarded the Titanic
+# from Southampton, we'll assign the `Embarked` as `S` for these passengers.
+
+# %%
+df["embarked"] = df["embarked"].fillna(value="S")
+df["embarked"].isna().sum()
+
+
+# %% [markdown]
+# ### Age
+
+# %% [markdown]
+# The dataset has 177 missing age values. Let's first investigate missing
+# values in the `age` column per title.
+
+# %%
+na_age_titles = df.loc[df.age.isna(), "title"].unique()
+print(na_age_titles)
+
+# %%
+df.loc[df.title.isin(na_age_titles), "title"].value_counts()
+
+# %% [markdown]
+# We see that the titles with missing age values contain nonempty entries as
+# well. Therefore, we can use groupings to impute the missing age entries. For
+# the majority of the cases, we can just use the medians to fill the missing
+# values. However, we should handle some cases manually. For example, Miss.
+# title includes girls and unmarried woman and first class passengers often
+# traveled with the family maids who were often unmarried women. Therefore,
+# we'll determine some cases to handle first.
+#
+# For titles Dr. and Master., we can just fill in by using title medians. Let's
+# start with that first.
+
+# %%
+dr_median = df.loc[df.title == "Dr.", "age"].median()
+df.loc[(df.title == "Dr.") & (df.age.isna()), "age"] = dr_median
+
+master_median = df.loc[df.title == "Master.", "age"].median()
+df.loc[(df.title == "Master.") & (df.age.isna()), "age"] = master_median
+
+# %% [markdown]
+# Next up, let's manually fill missing the young girl ages. We can filter by
+# title Miss. and `parch`. Recall that `parch` denotes the number of parents
+# or children onboard. Since Miss. title is exclusive to unmarried women and
+# having children without marriage was not common at all at the time, we can
+# assume that `parch` > 0 implies they have parents, which means that they are
+# girls or young women.
+
+# %%
+mask = (df.title == "Miss.") & (df.parch > 0)
+young_girl_median = df.loc[mask, "age"].median()
+df.loc[mask & (df.age.isna()), "age"] = young_girl_median
+
+# %% [markdown]
+# For the rest of the passengers, we can impute values with title and passenger
+# class medians. The reason we include passenger class is simple. First-class
+# passengers were generally wealthy businessmen, who are generally middle-aged
+# or older, and third-class passengers were usually workers and immigrants, who
+# are younger.
+
+# %%
+df["age"] = df["age"].fillna(
+    df.groupby(by=["title", "pclass"])["age"].transform("median")
+)
+df.age.isna().sum()
+
+# %% [markdown]
+# <div class="alert alert-block alert-info">
+#     <b>NOTE:</b> Although our methodology was solid, we may have introduced
+#     some noise to the dataset. In future versions, if a more careful
+#     procedure for preparing the data for model training is deemed necessary,
+#     this section can be improved.
+#
+# Here are some improvement options:
+#
+# 1. Refine the method
+# 2. Manually fill as much data as possible (some missing values from
+# the dataset can be found by research)
+# 3. Automation or web scraping for filling as much data as possible.
+#
+# </div>
+#
+# Titanic is has been a research area for many years and through the collective
+# efforts of many brilliant minds, a lot of the missing data has been
+# recovered. Therefore, we can fill the missing passenger ages in case they
+# are known but not present in our dataset. This kind of improvement is out of
+# our scope though.
