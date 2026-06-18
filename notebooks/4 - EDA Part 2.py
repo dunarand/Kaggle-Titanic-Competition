664758e842114789c4be62b18e84648fbf93fbb3
dunarand <dunarand@protonmail.com>
2026-06-18 20:59:06 +0300

    Initial EDA Part 2 Commit

---
+++ notebooks/4 - EDA Part 2.py
@@ -0,0 +1,32 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: title,-all
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.19.1
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # 4 - Exploratory Data Analysis Part 2
+
+
+# %% [markdown]
+# **Author:** M. Görkem Ulutürk
+#
+# **Date:** June, 2026
+
+
+# %% [markdown]
+# ## Introduction
+
+
+# %% [markdown]
+# ## Multivariate Analysis
